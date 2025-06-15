1cd4769e1071a411ab7ab781a41661fc16f23b4c
Farothtc <mvkirimi@gmail.com>
2025-06-15 18:20:17 +0300

    feat: API calls are inserted and implemented on UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,93 @@
 "use client";
+import axios from "axios";
 import Image from "next/image";
-import { use, useState } from "react";
+import { useState, useEffect } from "react";
 
 type IPData = {
   ip: string;
-  location: string;
+  region: string;
+  city: string;
+  postalCode: string;
   time: string;
   isp: string;
 };
 
 export default function Home() {
   const [ipData, setIpData] = useState<IPData>({
-    ip: "192.212.174.101",
-    location: "Brooklyn, NY 10001",
-    time: "UTC-05:00",
-    isp: "SpaceX Starlink",
+    ip: "",
+    region: "",
+    city: "",
+    postalCode: "",
+    time: "",
+    isp: "",
   });
-  const handleClick = (e: any) => {
-    e.preventDefault();
+  const [formData, setFormData] = useState({
+    userInput: "",
+  });
+
+  useEffect(() => {
+    const fetchFirst = async () => {
+      try {
+        const firstRes = await axios.get("https://api.ipify.org?format=json");
+        if (firstRes) {
+          const userIP = firstRes.data.ip;
+          const userGeo = `https://geo.ipify.org/api/v2/country,city?apiKey=at_n9MEFhzieAcbGWXa7TeI0rnbk2KTq&ipAddress=${userIP}`;
+          try {
+            const res = await axios.get(userGeo);
+            setIpData((prev) => ({
+              ...prev,
+              ip: res.data.ip,
+              region: res.data.location.region,
+              city: res.data.location.city,
+              postalCode: res.data.location.postalCode,
+              time: res.data.location.timezone,
+              isp: res.data.isp,
+            }));
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchFirst();
+  }, []);
+
+  const isIP = (e: string) => {
+    const ipv4 =
+      /^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3}$/;
+    const ipv6 = /^([a-fA-F0-9:]+:+)+[a-fA-F0-9]+$/;
+    return ipv4.test(e) || ipv6.test(e);
   };
+
+  const handleClick = async (e: any) => {
+    e.preventDefault();
+    const userInputGeo = formData.userInput;
+    let searchGeo = "";
+    if (isIP(userInputGeo)) {
+      searchGeo = `https://geo.ipify.org/api/v2/country,city?apiKey=at_n9MEFhzieAcbGWXa7TeI0rnbk2KTq&ipAddress=${userInputGeo}`;
+    } else {
+      searchGeo = `https://geo.ipify.org/api/v2/country,city?apiKey=at_n9MEFhzieAcbGWXa7TeI0rnbk2KTq&domain=${userInputGeo}`;
+    }
+    try {
+      const res = await axios.get(searchGeo);
+      setIpData((prev) => ({
+        ...prev,
+        ip: res.data.ip,
+        region: res.data.location.region,
+        city: res.data.location.city,
+        postalCode: res.data.location.postalCode,
+        time: res.data.location.timezone,
+        isp: res.data.isp,
+      }));
+      console.log(res.data.location.postalCode);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const cardTitle = "card-title text-sm text-gray-400 tracking-widest";
+  const cardDataCss = "text-black font-semibold text-2xl";
 
   return (
     <main className="min-h-screen">
@@ -33,6 +102,11 @@ export default function Home() {
               <div className="join w-[35%]">
                 <input
                   type="text"
+                  name="userInput"
+                  value={formData.userInput}
+                  onChange={(e) =>
+                    setFormData({ ...formData, userInput: e.target.value })
+                  }
                   placeholder="Search for any IP address or domain"
                   className="input w-full h-14 rounded-s-xl bg-white placeholder:text-gray-400 placeholder:ps-2 placeholder:text-lg text-black"
                 />
@@ -53,23 +127,46 @@ export default function Home() {
         </div>
       </section>
       <div className="absolute top-[22%] left-1/2 -translate-x-1/2 z-10 w-[80%]">
-        <div className="card w-full h-42 bg-white text-black card-xl shadow-sm">
-          <div className="card-body flex flex-row justify-between items-center">
-            <div>
-              <h2 className="card-title">Xlarge Card</h2>
-              <h2>{ipData.ip}</h2>
+        <div className="card w-full h-44 bg-white text-black card-xl shadow-sm">
+          <div className="card-body flex flex-row justify-evenly items-start">
+            <div className="flex flex-col gap-3">
+              <h2 className={`${cardTitle}`}>IP ADDRESS</h2>
+              {ipData.ip !== "" ? (
+                <h2 className={`${cardDataCss}`}>{ipData.ip}</h2>
+              ) : (
+                <span className="loading loading-spinner loading-xl"></span>
+              )}
             </div>
-            <div>
-              <h2 className="card-title">Xlarge Card</h2>
-              <h2>{ipData.location}</h2>
+            <hr className="w-0.5 h-full bg-gray-200 border-none mx-1" />
+            <div className="flex flex-col gap-3">
+              <h2 className={`${cardTitle}`}>LOCATION</h2>
+              {ipData.region !== "" ? (
+                <h2 className={`${cardDataCss}`}>
+                  {ipData.region}, {ipData.city}
+                  <br />
+                  {ipData.postalCode}
+                </h2>
+              ) : (
+                <span className="loading loading-spinner loading-xl"></span>
+              )}
             </div>
-            <div>
-              <h2 className="card-title">Xlarge Card</h2>
-              <h2>{ipData.time}</h2>
+            <hr className="w-0.5 h-full bg-gray-200 border-none mx-1" />
+            <div className="flex flex-col gap-3">
+              <h2 className={`${cardTitle}`}>TIMEZONE</h2>
+              {ipData.time !== "" ? (
+                <h2 className={`${cardDataCss}`}>{"UTC" + ipData.time}</h2>
+              ) : (
+                <span className="loading loading-spinner loading-xl"></span>
+              )}
             </div>
-            <div>
-              <h2 className="card-title">Xlarge Card</h2>
-              <h2>{ipData.isp}</h2>
+            <hr className="w-0.5 h-full bg-gray-200 border-none mx-1" />
+            <div className="flex flex-col gap-3">
+              <h2 className={`${cardTitle}`}>ISP</h2>
+              {ipData.isp !== "" ? (
+                <h2 className={`${cardDataCss}`}>{ipData.isp}</h2>
+              ) : (
+                <span className="loading loading-spinner loading-xl"></span>
+              )}
             </div>
           </div>
         </div>
